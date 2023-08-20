4312161eecb070c2cfec0f3aab64682e27722a48
josafatbadilla <josafatbadilla@estudiantec.cr>
2023-08-20 11:48:30 -0600

    Agregar archivos de proyecto.

---
+++ Tarea1BD/Pages/Articulos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tarea1BD.Pages
+{
+    public class ArticulosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
